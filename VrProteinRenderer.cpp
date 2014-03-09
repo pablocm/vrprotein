84178fc212ca779f652e90dc12d96cf585544bae
Pablo Cruz <pcruzm@gmail.com>
2014-03-09 20:45:51 -0300

    Formatting.

--- VrProteinRenderer.cpp
+++ VrProteinRenderer.cpp
@@ -1,22 +1,21 @@
 /***********************************************************************
-VrProteinRenderer - Extremely simple Vrui application to demonstrate the
-small amount of code overhead introduced by the Vrui toolkit.
-Copyright (c) 2006-2013 Oliver Kreylos
+ VrProteinRenderer
+ Copyright (c) 2014 Pablo Cruz
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the
-Free Software Foundation; either version 2 of the License, or (at your
-option) any later version.
+ This program is free software; you can redistribute it and/or modify it
+ under the terms of the GNU General Public License as published by the
+ Free Software Foundation; either version 2 of the License, or (at your
+ option) any later version.
 
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
 
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-***********************************************************************/
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ ***********************************************************************/
 
 #include <GL/gl.h>
 #include <GL/GLMaterialTemplates.h>
@@ -24,43 +23,52 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <Vrui/Vrui.h>
 #include <Vrui/Application.h>
 
-class VrProteinRenderer:public Vrui::Application
-	{
-	/* Constructors and destructors: */
-	public:
-	VrProteinRenderer(int& argc,char**& argv);
-	
+class VrProteinRenderer: public Vrui::Application {
+public:
+	VrProteinRenderer(int& argc, char**& argv);
+
 	/* Methods from Vrui::Application: */
 	virtual void display(GLContextData& contextData) const;
-	};
+	virtual void frame();
+};
 
 /******************************
-Methods of class VrProteinRenderer:
-******************************/
+ Methods of class VrProteinRenderer:
+ ******************************/
 
-VrProteinRenderer::VrProteinRenderer(int& argc,char**& argv)
-	:Vrui::Application(argc,argv)
-	{
+VrProteinRenderer::VrProteinRenderer(int& argc, char**& argv) :
+		Vrui::Application(argc, argv) {
 	/* Set the navigation transformation to show the entire scene: */
-	Vrui::setNavigationTransformation(Vrui::Point::origin,Vrui::Scalar(12));
-	}
+	Vrui::setNavigationTransformation(Vrui::Point::origin, Vrui::Scalar(12));
+}
 
-void VrProteinRenderer::display(GLContextData& contextData) const
-	{
+void VrProteinRenderer::display(GLContextData& contextData) const {
 	/* Draw a red cube and a blue sphere: */
 	glPushMatrix();
-	
-	glTranslated(-5.0,0.0,0.0);
-	glMaterialAmbientAndDiffuse(GLMaterialEnums::FRONT,GLColor<GLfloat,4>(1.0f,0.5f,0.0f));
+
+	glTranslated(-5.0, 0.0, 0.0);
+	glMaterialAmbientAndDiffuse(GLMaterialEnums::FRONT, GLColor<GLfloat, 4>(1.0f, 0.5f, 0.0f));
 	//glDrawCube(7.5f);
 	glDrawWireframeCube(7.5f, 1.0f, 1.5f);
-	
-	glTranslated(10.0,0.0,0.0);
-	glMaterialAmbientAndDiffuse(GLMaterialEnums::FRONT,GLColor<GLfloat,4>(0.5f,0.5f,1.0f));
-	glDrawSphereIcosahedron(4.5f,6);
-	
+
+	glTranslated(10.0, 0.0, 0.0);
+	glMaterialAmbientAndDiffuse(GLMaterialEnums::FRONT, GLColor<GLfloat, 4>(0.5f, 0.5f, 1.0f));
+	glDrawSphereIcosahedron(4.5f, 6);
+
 	glPopMatrix();
-	}
+}
+
+void VrProteinRenderer::frame() {
+}
 
 /* Create and execute an application object: */
-VRUI_APPLICATION_RUN(VrProteinRenderer)
+int main(int argc, char* argv[]) {
+	try {
+		VrProteinRenderer app(argc, argv);
+		app.run();
+	} catch (std::runtime_error &err) {
+		std::cerr << "Terminated program due to exception: " << err.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
